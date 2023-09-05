8774636cb315b30f5a98e3729a21b25767890721
Muhammad Ryan Pranata <pranataryan91@gmail.com>
2023-09-05 11:34:30 +0700

    Menambahkan Header dan Navbar

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,5 +1,40 @@
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { navLinks } from '../data/index';
+import { NavLink } from 'react-router-dom';
+
 const NavbarComponent = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <Navbar expand='lg'>
+        <Container>
+          <Navbar.Brand className='fs-3 fw-bold' href='#home'>React E-Course</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='mx-auto'>
+              {navLinks.map((link) => {
+                return (
+                  <div className='nav-link' key={link.id}>
+                    <NavLink
+                      to={link.path}
+                      className={({ isActive, isPending }) =>
+                        isPending ? 'pending' : isActive ? 'active' : ''
+                      } end
+                    >
+                      {link.text}
+                    </NavLink>
+                  </div>
+                );
+              })}
+            </Nav>
+
+            <div >
+              <button className='btn btn-outline-danger rounded-1'>Join With Us</button>
+            </div>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 };
 
 export default NavbarComponent;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,9 @@
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div className='homepage'>
+      <header className="w-100 min-vh-100 vh-100"></header>
+    </div>
+  );
 };
 
 export default Homepage;
