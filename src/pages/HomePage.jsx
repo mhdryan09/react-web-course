1afb977c31c2095ece6dc0b50adc61c8eeb625ee
Muhammad Ryan Pranata <pranataryan91@gmail.com>
2023-09-05 14:34:48 +0700

    Membuat Section Homepage Header

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,33 @@
+import { Container, Row, Col } from 'react-bootstrap';
+import HeroImage from '../assets/img/hero.png';
 const Homepage = () => {
   return (
     <div className="homepage">
-      <header className="w-100 min-vh-100"></header>
+      <header className="w-100 min-vh-100 d-flex align-items-center">
+        <Container>
+          <Row className="header-box d-flex align-items-center">
+            <Col lg="6">
+              <h1 className="mb-4">
+                Temukan <br /> <span> Bakat Kreatifmu</span> <br /> bersama
+                Kami!
+              </h1>
+              <p className="mb-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure,
+                ab? Reiciendis inventore quibusdam excepturi incidunt.
+              </p>
+              <button className="btn btn-danger btn-lg rounded-1 me-2 mb-xs-0 mb-2">
+                Lihat Kelas
+              </button>
+              <button className="btn btn-outline-danger btn-lg rounded-1 mb-xs-0 mb-2">
+                Lihat Promo
+              </button>
+            </Col>
+            <Col lg="6" className="pt-lg-0 pt-5">
+              <img src={HeroImage} alt="Hero Image" />
+            </Col>
+          </Row>
+        </Container>
+      </header>
       <div className="kelas w-100 min-vh-100"></div>
     </div>
   );
