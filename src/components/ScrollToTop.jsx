559892b96d24fd3fb54756cd8304ee9a7b7372ea
Muhammad Ryan Pranata <pranataryan91@gmail.com>
2023-09-10 12:02:34 +0700

    Menambahkan Scroll To Top

---
+++ src/components/ScrollToTop.jsx
@@ -0,0 +1,10 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0); // scroll to top 
+  }, [pathname]); // only scroll to top when pathname changes 
+}
--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,12 @@ import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './main.css';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
